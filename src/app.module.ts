2a61f95579ef187bae8cb3417d9241f74eb6e025
felixyunianto <felixyunianto@gmail.com>
2021-09-03 09:38:52 +0700

    3. Update

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,20 @@ import { ProductsService } from './products/products.service';
 import { ProductsController } from './products/products.controller';
 import { ProductsModule } from './products/products.module';
 import { MulterModule } from '@nestjs/platform-express';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), ProductsModule, MulterModule.register({
-    dest : './images'
-  })],
+  imports: [
+    TypeOrmModule.forRoot(),
+    ProductsModule,
+    MulterModule.register({
+      dest: './images',
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+  ],
   controllers: [AppController, ProductsController],
   providers: [AppService, ProductsService],
 })
